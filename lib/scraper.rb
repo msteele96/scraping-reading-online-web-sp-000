9fc0b256555cd015579cb94a725f3f07bcbbfa38
Marc Steele <marcsteele2014@gmail.com>
2020-11-02 18:01:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN").text
 
-courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+courses = doc.css("inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|
   puts course.text.strip
